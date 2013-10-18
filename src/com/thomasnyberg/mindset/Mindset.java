8ea301cf5264b0568ccb1263508e699d76eb80cd
Thomas W Nyberg <tomnyberg@gmail.com>
2013-10-18 19:28:24 -0400

    Bluetooth Step 1
    Now have some basic querying of the Bluetooth adapter. Started trying
    some logic handling (is Bluetooth turned off? etc.), but used a handler
    for the messaging which is too fast...need to do something that actually
    blocks.

--- src/com/thomasnyberg/mindset/Mindset.java
+++ src/com/thomasnyberg/mindset/Mindset.java
@@ -9,10 +9,14 @@ import java.lang.Runnable;
 import java.lang.Thread;
 import android.util.Log;
 import android.os.Message;
+import android.content.Intent;
+
+import android.bluetooth.BluetoothAdapter;
 
 public class Mindset extends Activity
 {
     TextView mainWindow;
+    BluetoothAdapter myBluetoothAdapter;
 
     Handler handler = new Handler() {
         @Override
@@ -53,7 +57,67 @@ public class Mindset extends Activity
                 }
             }
         });
+
+        Message msg = new Message();
+        Bundle b = new Bundle();
+        myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (myBluetoothAdapter == null) {
+            b.putString("My Key", "Bluetooth Borked");
+            msg.setData(b);
+        } else {
+            b.putString("My Key", "Bluetooth Success!");
+            msg.setData(b);
+        }
+        handler.sendMessage(msg);
+
+        /* All these handlers are not happy...possibly too many happending too
+         * quickly? */
+        if (!myBluetoothAdapter.isEnabled()) {
+            b.putString("My Key", "Bluetooth Turned Off");
+            msg.setData(b);
+            /*handler.sendMessage(msg);*/
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            int REQUEST_ENABLE_BT = 1;
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+
+            if (myBluetoothAdapter.isEnabled()) {
+                b.putString("My Key", "Bluetooth Now Turned On");
+            } else {
+                b.putString("My Key", "Fucker Said No");
+            }
+            msg.setData(b);
+            /*handler.sendMessage(msg);*/
+        } else {
+            b.putString("My Key", "Bluetooth Already Turned On");
+            msg.setData(b);
+            /*handler.sendMessage(msg);*/
+        }
+
+
+
+        /* The following gives an error and I don't really understand why:
+         
+        Thread bluetooth = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Message msg = new Message();
+                Bundle b = new Bundle();
+                myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+                if (myBluetoothAdapter == null) {
+                    b.putString("My Key", "Bluetooth Borked");
+                    msg.setData(b);
+                } else {
+                    b.putString("My Key", "Bluetooth Success!");
+                    msg.setData(b);
+                }
+                handler.sendMessage(msg);
+            }
+        });
         
+        */
+
         background.start();
+
+        /*bluetooth.start();*/
     }
 }
