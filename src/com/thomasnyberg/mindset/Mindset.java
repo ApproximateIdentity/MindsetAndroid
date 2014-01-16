ac97c2ab9a9488bbb174beec0586acd656bffc25
Thomas Nyberg <tomnyberg@gmail.com>
2014-01-16 17:39:24 -0500

    Implement basic line scrolling
    The main UI thread now prints a new line every second to a TextView.
    When it reaches the bottom, the TextView scrolls down.
    
    The current implementation iteratively concatenates a string which is
    then printed out in its entirety before the scrolling fixes everything
    up. Thus the computations necessary to print each new line increases
    linearly in the line number. Since the printing is all being done in the
    UI thread, the UI thread computation time increases linearly in the line
    number, whereas the AsyncTask background thread's computation time is
    constant in the line number.
    
    This is obviously a bad implementation and it's not hypothetical, since
    the UI thread slows down noticably in under 1000 lines. An obvious fix
    is to use a sort of frame buffer which should contain a fixed upper
    bound of text strings representing lines. This fix alone would make the
    UI thread run in constant time in the line number.

--- src/com/thomasnyberg/mindset/Mindset.java
+++ src/com/thomasnyberg/mindset/Mindset.java
@@ -4,25 +4,60 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import android.os.AsyncTask;
+import java.lang.Thread;
+
+import android.text.method.ScrollingMovementMethod;
+import android.text.Layout;
+
 public class Mindset extends Activity {
   TextView mainWindow;
 
-  /** Called when the activity is first created. */
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
     mainWindow = (TextView) findViewById(R.id.mainWindow);
+    mainWindow.setMovementMethod(new ScrollingMovementMethod());
   }
 
   @Override
   protected void onStart() {
     super.onStart();
 
-    for (int i = 0; i < 5; i++) {
+    new UpdateGUI().execute();
+  }
+
+  private class UpdateGUI extends AsyncTask<Void, Integer, Void> {
+    protected Void doInBackground(Void... params) {
+      int i = 0;
+      for (;;) {
+        try {
+          Thread.sleep(1000);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+        publishProgress(++i);
+      }
+    }
+
+    protected void onProgressUpdate(Integer... progress) {
+      Layout layout = mainWindow.getLayout();
+
+      int lineCount = mainWindow.getLineCount();
+      int windowHeight = mainWindow.getHeight();
+      int lineTop = layout.getLineTop(lineCount);
+
+      int scrollAmount = layout.getLineTop(lineCount) - windowHeight;
+
+      if (scrollAmount > 0) {
+        mainWindow.scrollTo(0, scrollAmount);
+      } else {
+        mainWindow.scrollTo(0, 0);
+      }
+
       CharSequence text = mainWindow.getText();
-      text += "\nShit ";
-      text += Integer.toString(i);
+      text += "Line: " + Integer.toString(lineCount) + "\n";
       mainWindow.setText(text);
     }
   }
