cfa77cdffbd6e68c4e17293d422ac3a39a8511e6
Thomas Nyberg <tomnyberg@gmail.com>
2014-01-18 12:33:28 -0500

    Add basic Bluetooth connection capability
    This runs without crashing (yes!) and seemed to successfully connect to
    Mindset once. One (now) obvious problem is that the connect method
    returns without setting a global socket value in the class meaning that
    the connections are destroyed when the function returns. Needs fixing...

--- src/com/thomasnyberg/mindset/BluetoothConn.java
+++ src/com/thomasnyberg/mindset/BluetoothConn.java
@@ -2,6 +2,11 @@ package com.thomasnyberg.mindset;
 
 import java.lang.Thread;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import java.util.UUID;
+import android.bluetooth.BluetoothSocket;
+
+import java.io.IOException;
 
 public class BluetoothConn {
   BluetoothAdapter myBluetoothAdapter = null;
@@ -25,7 +30,39 @@ public class BluetoothConn {
     }
   }
 
-  public void connect() {}
+  public void connect() {
+    BluetoothDevice mindset = null;
+    BluetoothSocket btSock = null;
+
+    /* The following is my Mindset's address. */
+    final String mindsetAddress = "00:13:EF:00:3B:F6";
+
+    try {
+      mindset = myBluetoothAdapter.getRemoteDevice(mindsetAddress);
+    } catch (IllegalArgumentException e) {
+      e.printStackTrace();
+    }
+
+    /* The following string is the UUID for the serial port profile. */
+    final String uuidString = "00001101-0000-1000-8000-00805F9B34FB";
+    UUID uuid = UUID.fromString(uuidString);
+
+    if (mindset != null) {
+      try {
+        btSock = mindset.createRfcommSocketToServiceRecord(uuid);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+
+    if (btSock != null) {
+      try {
+        btSock.connect();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
 
   private int counter = 1;
   public String getData() {
