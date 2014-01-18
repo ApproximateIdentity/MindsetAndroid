0111af727cc34cf8bd697b0d48b60ac7ffde415d
Thomas Nyberg <tomnyberg@gmail.com>
2014-01-18 18:19:15 -0500

    Fix use of Bluetooth resources
    Now the app connects to the Mindset upon start up and disconnects when
    stopped or killed. After being stopped, the app reaquires the resources
    correctly when starting back up. After being killed, it starts back up
    from the beginning as it should. If any steps during startup or restart
    up fail, an error description is printed and the app is halted entirely.

--- src/com/thomasnyberg/mindset/Mindset.java
+++ src/com/thomasnyberg/mindset/Mindset.java
@@ -4,18 +4,32 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.bluetooth.BluetoothAdapter;
+import java.util.UUID;
+import android.bluetooth.BluetoothSocket;
+import android.bluetooth.BluetoothDevice;
 
 import android.os.AsyncTask;
 import android.content.Intent;
 
+import java.io.IOException;
+import java.lang.Thread;
 
 public class Mindset extends Activity {
-  Terminal term;
+  private boolean HALT = false;
+  private Terminal term;
+  private BluetoothAdapter myBluetoothAdapter;
+  private final String mindsetAddress = "00:13:EF:00:3B:F6";
+  private BluetoothDevice mindset;
+  private final String uuidString = "00001101-0000-1000-8000-00805F9B34FB";
+  private BluetoothSocket sock;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
+
+    /* Initialize main window terminal. */
+    /* Here I apparently am assuming this operation will never fail... */
     term = new Terminal((TextView) findViewById(R.id.mainWindow));
 
     /* I am making sure the BluetoothAdapter is turned on in the UI thread
@@ -29,27 +43,105 @@ public class Mindset extends Activity {
           "bluetoothadapter-getdefault-throwing" +
           "-runtimeexception-while-not-in-activity"
     */
-    BluetoothAdapter myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     if (myBluetoothAdapter == null) {
-      term.writeLine("Error: BluetoothAdapter");
-    } else if (!myBluetoothAdapter.isEnabled()) {
-      Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE); 
-      int REQUEST_ENABLE_BT = 1;
-      startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-
-      new MindsetDataStream().execute();
+      term.writeLine("Error: Bluetooth is not supported on this platform.");
+      HALT = true;
+      return;
     }
 
+    if (!myBluetoothAdapter.isEnabled()) {
+      term.writeLine("Bluetooth currently disabled.");
+      term.writeLine("Attempting to enable Bluetooth.");
+      Intent enableBt = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE); 
+      int REQUEST_ENABLE_BT = 1;
+      startActivityForResult(enableBt, REQUEST_ENABLE_BT);
+    }
+
+    /* The enabling of Bluetooth is asynchronous which I do not like. This
+       blocks until Bluetooth is enabled. */
+    for (int i = 0; i < 120; i++) {
+      wasteSomeTime(500);
+      if (myBluetoothAdapter.isEnabled()) {
+        break;
+      }
+    }
+
+    if (!myBluetoothAdapter.isEnabled()) {
+      term.writeLine("Error: Attempt at enabling Bluetooth timed out.");
+      HALT = true;
+      return;
+    } else {
+      term.writeLine("Successfully enabled Bluetooth.");
+    }
+
+    term.writeLine("Attempting to find Mindset device.");
+    try {
+      mindset = myBluetoothAdapter.getRemoteDevice(mindsetAddress);
+    } catch (IllegalArgumentException e) {
+      term.writeLine("Could not find Mindset device.");
+      HALT = true;
+      return;
+    }
+    term.writeLine("Successfully found Mindset device.");
   }
 
   @Override
   protected void onStart() {
     super.onStart();
+
+    if (HALT) {
+      term.writeLine("Error: General error.");
+      term.writeLine("HALT.");
+      return;
+    }
+
+    /* Create RFComm socket connection with Mindset. */
+    /* The following string is the UUID for the serial port profile. */
+    UUID uuid = UUID.fromString(uuidString);
+    try {
+      sock = mindset.createRfcommSocketToServiceRecord(uuid);
+    } catch (IOException e) {
+      term.writeLine("Error: Could not create RFComm socket.");
+      HALT = true;
+      return;
+    }
+
+    term.writeLine("Attempting to connect to Mindset.");
+    try {
+      sock.connect();
+    } catch (IOException e) {
+      term.writeLine("Could not connect to Mindset.");
+      HALT = true;
+      return;
+    }
+    term.writeLine("Successfully connected to Mindset.");
+    /*new MindsetDataStream().execute();*/
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    try {
+      sock.close();
+    } catch (IOException e) {
+      term.writeLine("Error: Could not close Mindset connection.");
+      HALT = true;
+      return;
+    }
   }
 
   @Override
   protected void onResume() {
     super.onResume();
+  }
+
+  private void wasteSomeTime(int msec) {
+    try {
+      Thread.sleep(msec);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
   }
 
   private class MindsetDataStream extends AsyncTask<Void, String, Void> {
@@ -58,7 +150,7 @@ public class Mindset extends Activity {
     protected Void doInBackground(Void... params) {
       publishProgress("Attempting to connect to Mindstream.");
 
-      BluetoothConn conn = new BluetoothConn("Mindset");
+      BluetoothConn conn = new BluetoothConn();
       conn.connect();
       if (conn.error) {
         publishProgress("Error: " + conn.errorString);
